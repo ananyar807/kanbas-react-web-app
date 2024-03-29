1cdd7389728329625e31279155c887075a16c56e
Ananya Radhakrishnan <ananyar807@gmail.com>
2024-03-29 15:06:08 -0400

    updated api to be render

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -11,7 +11,8 @@ const API_BASE = process.env.REACT_APP_API_BASE;
 
 function Kanbas() {
   const [courses, setCourses] = useState<any[]>([]);
-  const COURSES_API = `${API_BASE}/api/courses`;
+  const COURSES_API =
+    "https://kanbas-node-server-app-dsa7.onrender.com/api/courses";
   const findAllCourses = async () => {
     const response = await axios.get(COURSES_API);
     setCourses(response.data);
